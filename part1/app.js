a55af15cdd97d0db3fad67ea4ef953b18840d97c
koeroessy <a1924871@adelaide.edu.au>
2025-06-20 05:47:04 +0000

    [automatic commit] 2025-06-20 05:47:04 UTC 175.35.111.14

--- part1/app.js
+++ part1/app.js
@@ -93,4 +93,17 @@ let db;
   }
 })();
 
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [rows] = await db.execute(`
+      SELECT Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      FROM Dogs
+      JOIN Users ON Dogs.owner_id = Users.user_id
+    `);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
 module.exports = app;
