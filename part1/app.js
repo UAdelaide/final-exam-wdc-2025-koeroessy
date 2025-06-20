771a39a9bf6e1be81d1b07db3ed169d083921eca
koeroessy <a1924871@adelaide.edu.au>
2025-06-20 05:46:44 +0000

    [automatic commit] 2025-06-20 05:46:44 UTC 175.35.111.14

--- part1/app.js
+++ part1/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mysql = require('mysql2/promise');
 
 var app = express();
 
